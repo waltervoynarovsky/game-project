1e033fe1bcce7c57fd0029a14ce075214736b9c8
Walter Wojnarowski <walterwojnarowski@hotmail.com>
2022-11-30 10:14:43 +0100

    more questions added, input changed to radio

--- questions.js
+++ questions.js
@@ -1,114 +1,155 @@
 const questionsArray = [
   {
     text: "does this game work?",
-    a: "no",
-    b: "maybe",
-    c: "maybe not",
-    d: "maybe yes",
-    right: "no",
+    options: {
+      a: "no",
+      b: "maybe",
+      c: "maybe not",
+      d: "maybe yes",
+    },
+    right: "maybe yes",
   },
 
   {
-    text: "is the lord of the rings?",
-    a: "no",
-    b: "maybenot",
-    c: "maybeyes",
-    d: "maybe maybe",
-    right: "maybe maybe",
+    text: "Upon coronation, what name did Aragon take?",
+    options: {
+      a: "Amandil",
+      b: "Arathorn",
+      c: "Elendil",
+      d: "Elessar",
+    },
+    right: "Elessar",
   },
   {
     text: "What was the name of Tom Bombadil's wife",
-    a: "Goldberry",
-    b: "River",
-    c: "Rose",
-    d: "Lavender",
+    options: {
+      a: "Goldberry",
+      b: "River",
+      c: "Rose",
+      d: "Lavender",
+    },
     right: "Goldberry",
   },
   {
     text: "What was Gandalf the Grey called by elves",
-    a: "Mithrandir",
-    b: "Istari",
-    c: "Elendil",
-    d: "Blue wizard",
+    options: {
+      a: "Mithrandir",
+      b: "Istari",
+      c: "Elendil",
+      d: "Blue wizard",
+    },
     right: "Mithrandir",
   },
   {
     text: "Was Thranduil a:",
-    a: "King",
-    b: "Prince",
-    c: "Duke",
-    d: "Steward",
+    options: {
+      a: "King",
+      b: "Prince",
+      c: "Duke",
+      d: "Steward",
+    },
+    right: "Prince",
   },
   {
-    text: "Who from the Fellowship of the Ring became an 'Esquire of Rohan'",
-    a: "Meriadoc Brandybuck",
-    b: "Perregrin Tuk",
-    c: "Samwise Gamgee",
-    d: "Faramir II",
+    text: "Who from the Fellowship of the Ring became an 'Esquire of Rohan'?",
+    options: {
+      a: "Meriadoc Brandybuck",
+      b: "Perregrin Tuk",
+      c: "Samwise Gamgee",
+      d: "Faramir II",
+    },
+    right: "Meriadoc Brandybuck",
   },
   {
-    text: "Was Sauron a:",
-    a: "a",
-    b: "b",
-    c: "c",
-    d: "d",
+    text: "What do Merry and Pippin steal from Gandalf?",
+    options: {
+      a: "Muffins",
+      b: "Fireworks",
+      c: "Staff",
+      d: "Hat",
+    },
+    right: "Fireworks",
   },
-  //   {
-  //     text:,
-  //     a:,
-  //     b:,
-  //     c:,
-  //     d:,
-  //   }
-  //   {
-  //     text:,
-  //     a:,
-  //     b:,
-  //     c:,
-  //     d:,
-  //   }
-  //   {
-  //     text:,
-  //     a:,
-  //     b:,
-  //     c:,
-  //     d:,
-  //   }
-  //   {
-  //     text:,
-  //     a:,
-  //     b:,
-  //     c:,
-  //     d:,
-  //   }
-  //   {
-  //     text:,
-  //     a:,
-  //     b:,
-  //     c:,
-  //     d:,
-  //   }
-  //   {
-  //     text:,
-  //     a:,
-  //     b:,
-  //     c:,
-  //     d:,
-  //   }
-  //   {
-  //     text:,
-  //     a:,
-  //     b:,
-  //     c:,
-  //     d:,
-  //   }
-  //   {
-  //     text:,
-  //     a:,
-  //     b:,
-  //     c:,
-  //     d:,
-  //   }
+  {
+    text: "Who tries to destroy the Ring at the Council of Elrond?",
+    options: {
+      a: "Gandalf",
+      b: "Gimli",
+      c: "Aragorn",
+      d: "Legolas",
+    },
+    right: "Gimli",
+  },
+  {
+    text: "In The Return of the King, who kills the Witch King?",
+    options: {
+      a: "Arwen",
+      b: "Gandalf",
+      c: "Aragorn",
+      d: "Eowyn",
+    },
+    right: "Eowyn",
+  },
+  {
+    text: "What was the name of Sam's favorite pony?",
+    options: {
+      a: "Bill",
+      b: "Molly",
+      c: "Ted",
+      d: "Ernie",
+    },
+    right: "Billy",
+  },
+  {
+    text: "What is the name of Frodo's gardener?",
+    options: {
+      a: "Samwise Gamgee",
+      b: "Elanor Gardner",
+      c: "Bill Upperby",
+      d: "Odo Proudfoot",
+    },
+    right: "Samwise Gamgee",
+  },
+  {
+    text: "What is the name of Gimli's father?",
+    options: {
+      a: "Gloin",
+      b: "Balin",
+      c: "Durin",
+      d: "Thurin",
+    },
+    right: "Gloin",
+  },
+  {
+    text: "What was the name of the ancient sword that cut the One Ring from Sauron's hand?",
+    options: {
+      a: "Morgul-blade",
+      b: "Narsil",
+      c: "Glamdring",
+      d: "Sting",
+    },
+    right: "Narsil",
+  },
+  {
+    text: "What was the name of the elvish bread that was Frodo's and Sam's everyday food?",
+    options: {
+      a: "Lembas",
+      b: "Cram Bread",
+      c: "Miruvor",
+      d: "Tharbad",
+    },
+    right: "Lembas",
+  },
+  {
+    text: "Which forest was known for being the last habitation of the Ents and Huorns?",
+    options: {
+      a: "Mirkwood",
+      b: "Trollshaws",
+      c: "Fangorn",
+      d: "Valinor",
+    },
+    right: "Fangorn",
+  },
 ];
 
 export default questionsArray;
--- script.js
+++ script.js
@@ -2,20 +2,41 @@ import questionsArray from "./questions.js";
 
 const container = document.querySelector(".question-container");
 
+// function getRandomInt(max) {
+//   return Math.floor(Math.random() * max);
+// }
+
+// questionsArray.forEach((element) => {
+//   const randomizedAnswers = getRandomInt(element.options);
+//   return randomizedAnswers
+// });
+
 const generateQuestions = (questionsArray) => {
-  questionsArray.map((quest) => {
+  questionsArray.forEach((question) => {
     return (container.innerHTML += `<div class=question-card>
       <h2 class="card__heading">Question</h2>
-      <p class="question__text">${quest.text}</p>
-      <input type="checkbox" id="${quest.a}" name="answer-A">  
-      <label for="answer-A"> ${quest.a} </label>
-      <input type="checkbox" id="${quest.b}" name="answer-B">  
-      <label for="answer-B"> ${quest.b} </label>
-      <input type="checkbox" id="${quest.c}" name="answer-C">  
-      <label for="answer-C"> ${quest.c} </label>
-      <input type="checkbox" id="${quest.d}" name="answer-D">  
-      <label for="answer-D"> ${quest.d} </label>
+      <p class="question__text">${question.text}</p>
+      <input type="radio" id="${question.options.a}" name="answer">  
+      <label for="answer-A"> ${question.options.a} </label>
+      <input type="radio" id="${question.options.b}" name="answer${[]}">  
+      <label for="answer-B"> ${question.options.b} </label>
+      <input type="radio" id="${question.options.c}" name="answer${[]}">  
+      <label for="answer-C"> ${question.options.c} </label>
+      <input type="radio" id="${question.options.d}" name="answer${[]}">  
+      <label for="answer-D"> ${question.options.d} </label>
     </div> `);
   });
 };
 generateQuestions(questionsArray);
+
+for (const props in questionsArray.options) {
+  console.log(`${props}: ${questionsArray.options[props]}`);
+}
+
+// LOOP THROUGH THE OPTIONS AND CREATE
+
+// GOOGLE FOR IN (WHICH LOOPS OBJECTS)
+
+// OBJECT KETS AND OBJECT VALUES - WHICH ONE TO GRAB WHICH TO RANDOMIZE
+
+// ADD VALUE TO THE EVENT
